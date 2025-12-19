34a08059580e7485646beede638256a2a7936d4a
Md Intekhab Alam <intekhab8928@gmail.com>
2025-12-19 12:15:04 +0530

    changed the schema of user

--- src/features/product/category.schema.js
+++ src/features/product/category.schema.js
@@ -1,7 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 
 export const categorySchema = new Schema({
-    name: String,
     products: [
         {
             type:mongoose.Schema.Types.ObjectId,
